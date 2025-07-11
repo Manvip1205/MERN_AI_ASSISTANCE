8919ffcb567ef56dc38075dcb58bb9be8254c274
Manvip1205 <manvip2507@gmail.com>
2025-07-11 04:57:42 +0000

    frontend setup

---
+++ aiAssistance/src/App.jsx
@@ -0,0 +1,11 @@
+import LoginPage from "./pages/loginPage";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
