95ee60f659d0dd802437405b3226ebc43f4460d3
Manvip1205 <manvip2507@gmail.com>
2025-07-13 14:13:04 +0530

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,25 +2,25 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: {
-            type: String,
-            required: true
-        },
+        type: String,
+        required: true
+    },
     email: {
-            type: String,
-            required: true
-        },
+        type: String,
+        required: true
+    },
     password: {
-            type: String,
-            required: true
-        },
+        type: String,
+        required: true
+    },
     assistantName: {
-            type: String
-        },
+        type: String
+    },
     history: [{
-            type: String
-        }]
-}, { timestamps: true });  // ✅ fixed spelling too
+        type: String
+    }]
+}, { timestamps: true });
 
-                                                                                                        const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-                                                                                                        export default User;
+export default User;
